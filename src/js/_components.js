896adf0169c06dd7e34ae9ddf117f7521ddcd0a8
stor4 <storchak850@gmail.com>
2024-08-07 18:07:28 +0300

    inputs

--- src/js/_components.js
+++ src/js/_components.js
@@ -2,3 +2,4 @@ console.log('components');
 
 import './components/header.js';
 import './components/hero.js';
+import './components/potencial.js';
---
+++ src/js/components/potencial.js
@@ -0,0 +1,30 @@
+import $ from 'jquery';
+import 'ion-rangeslider';
+
+$(document).ready(function() {
+    $("#range1").ionRangeSlider({
+        type: "single",
+        min: 0,
+        max: 12,
+        from: 1,
+        to: 80,
+        grid: true,
+        postfix: " month",
+        // grid_snap: true,
+        grid_values: [0, 3, 6, 9, 12]
+    });
+});
+
+$(document).ready(function() {
+  $("#range2").ionRangeSlider({
+      type: "single",
+      min: 0,
+      max: 15000,
+      from: 2000,
+      to: 80,
+      grid: true,
+      postfix: " €",
+      // grid_snap: true,
+      grid_values: [300, 3975, 7650, 11325, 15000]
+  });
+});
