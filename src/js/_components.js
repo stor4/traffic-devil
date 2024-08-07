59ed591da1798412a3e4423452bf0d33b1d8acd7
stor4 <storchak850@gmail.com>
2024-08-08 00:29:29 +0300

    slider

--- src/js/_components.js
+++ src/js/_components.js
@@ -3,3 +3,4 @@ console.log('components');
 import './components/header.js';
 import './components/hero.js';
 import './components/potencial.js';
+import './components/slider.js';
---
+++ src/js/components/slider.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper(".slider__swiper", {
+  direction: "vertical",
+  slidesPerView: 1,
+  spaceBetween: 30,
+  mousewheel: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
